04b03da84542f69f53fcea70f8d7b62462bce646
Rüveyda Dal <ruveydadal.3@gmail.com>
2022-08-11 16:38:12 +0300

    Update Emailolusturma.java

--- Emailolusturma.java
+++ Emailolusturma.java
@@ -1,12 +1,9 @@
-
 package emailolusturma;
 
 public class Emailolusturma {
-
     public static void main(String[] args) {
+
         email i1=new email("Ahmet","Maşa");
-        
         System.out.println(i1.bilgi());
     }
-    
 }
